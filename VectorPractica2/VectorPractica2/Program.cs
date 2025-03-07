406eed9c7260f85cf3e517c315e9f7925d077d84
Ariany Lopez <arianylopez341@gmail.com>
2025-03-07 15:07:49 -0400

    Practica 2 de Vector - Recursividad

---
+++ VectorPractica2/VectorPractica2/Program.cs
@@ -0,0 +1,74 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresar la longitud del vector 1: ");
+        int longitud1 = int.Parse(Console.ReadLine());
+        Vector vector1 = new Vector(longitud1);
+
+        for (int i = 0; i < longitud1; i++)
+        {
+            Console.Write($"Elemento {i + 1}: ");
+            int valor = int.Parse(Console.ReadLine());
+            vector1.InsertarValor(valor, i, 0);
+        }
+        Console.WriteLine("Vector 1: ");
+        vector1.Mostrar();
+
+        Console.WriteLine("\nInsertar Elemento");
+        Console.Write("Ingresar Posicion: ");
+        int posicionInsertar = int.Parse(Console.ReadLine());
+        Console.Write("Ingresar Valor: ");
+        int valorInsertar = int.Parse(Console.ReadLine());
+        vector1.InsertarValor(valorInsertar, posicionInsertar, 0);
+        Console.WriteLine("Vector Modificado: ");
+        vector1.Mostrar();
+
+        Console.WriteLine("\nEliminar Elemento");
+        Console.Write("Ingresar Posicion: ");
+        int posicionEliminar = int.Parse(Console.ReadLine());
+        vector1.EliminarElemento(posicionEliminar, 0);
+        Console.WriteLine("Vector Modificado: ");
+        vector1.Mostrar();
+
+        Console.Write("\nIngresar longitud del vector 2: ");
+        int longitud2 = int.Parse(Console.ReadLine());
+        Vector vector2 = new Vector(longitud2);
+
+        for (int i = 0; i < longitud2; i++)
+        {
+            Console.Write($"Elemento {i + 1}: ");
+            int valor = int.Parse(Console.ReadLine());
+            vector2.InsertarValor(valor, i, 0);
+        }
+
+        bool sonIguales = vector1.CompararVectores(vector2);
+        Console.WriteLine("\nComparar Vectores");
+        Console.WriteLine("¿Los vectores son iguales? " + sonIguales);
+
+        Vector union = vector1.Union(vector2);
+        Console.Write("\nUnion de los vectores: ");
+        union.Mostrar();
+
+        bool subconjunto1 = vector1.Subconjunto(vector2);
+        bool subconjunto2 = vector2.Subconjunto(vector1);
+        Console.WriteLine("\n¿El vector1 es subconjunto del vector2? " + subconjunto1);
+        Console.WriteLine("\n¿El vector2 es subconjunto del vector1? " + subconjunto2);
+
+        Console.WriteLine("\nEliminar Duplicados");
+        Console.Write("Ingresar longitud para vector 3: ");
+        int longitud3 = int.Parse(Console.ReadLine());
+        Vector vector3 = new Vector(longitud3);
+
+        for (int i = 0; i < longitud3; i++)
+        {
+            Console.Write($"Elemento {i + 1}: ");
+            int valor = int.Parse(Console.ReadLine());
+            vector3.InsertarValor(valor, i, 0);
+        }
+
+        vector3.EliminarDuplicados();
+        Console.Write("\nVector sin duplicados: ");
+        vector3.Mostrar();
+    }
+}
---
+++ VectorPractica2/VectorPractica2/VectorPractica2.cs
@@ -0,0 +1,145 @@
+﻿class Vector
+{
+    private int[] elementos;
+    private int size;
+
+    public Vector(int longitud)
+    {
+        elementos = new int[longitud];
+        size = 0;
+    }
+
+    public void InsertarValor(int valor, int posicion, int index = 0)
+    {
+        if (size == elementos.Length)
+        {
+            int[] nuevoArr = new int[elementos.Length * 2];
+            for (int i = 0; i < size; i++)
+            {
+                nuevoArr[i] = elementos[i];
+            }
+            elementos = nuevoArr;
+        }
+
+        if (index == posicion)
+        {
+            for (int i = size; i > posicion; i--)
+            {
+                elementos[i] = elementos[i - 1];
+            }
+            elementos[posicion] = valor;
+            size++;
+        }
+        else
+        {
+            InsertarValor(valor, posicion, index + 1);
+        }
+    }
+
+    public void EliminarElemento(int posicion, int index = 0)
+    {
+        if (index == posicion)
+        {
+            for (int i = posicion; i < size - 1; i++)
+            {
+                elementos[i] = elementos[i + 1];
+            }
+            size--;
+        }
+        else
+        {
+            EliminarElemento(posicion, index + 1);
+        }
+    }
+
+    public bool CompararVectores(Vector nuevoVec)
+    {
+        if (size != nuevoVec.size)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            if (elementos[i] != nuevoVec.elementos[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Vector Union(Vector nuevoVec)
+    {
+        Vector uniones = new Vector(size + nuevoVec.size);
+
+        for (int i = 0; i < size; i++)
+        {
+            if (!uniones.contieneAux(elementos[i]))
+            {
+                uniones.InsertarValor(elementos[i], uniones.size, 0);
+            }
+        }
+
+        for (int i = 0; i < nuevoVec.size; i++)
+        {
+            if (!uniones.contieneAux(nuevoVec.elementos[i]))
+            {
+                uniones.InsertarValor(nuevoVec.elementos[i], uniones.size, 0);
+            }
+        }
+
+        return uniones;
+    }
+
+    public bool Subconjunto(Vector nuevoVec)
+    {
+        for (int i = 0; i < nuevoVec.size; i++)
+        {
+            if (!contieneAux(nuevoVec.elementos[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void EliminarDuplicados()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                if (elementos[i] == elementos[j])
+                {
+                    EliminarElemento(j, 0);
+                    j--;
+                }
+            }
+        }
+    }
+
+    private bool contieneAux(int valor)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (elementos[i] == valor)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Mostrar()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(elementos[i]);
+            if (i < size - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
